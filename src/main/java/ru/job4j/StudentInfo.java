74e1597a4b1734d4a8a094154dd7ca46ae1a019e
DethR <Dethrocker13@yandex.ru>
2022-09-14 22:12:39 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikolay Tsimbalyuk");
+        System.out.println("03.09.1993");
+    }
+}
