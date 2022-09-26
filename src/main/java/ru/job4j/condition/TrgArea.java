53b0e282a7365ba779da7b81afd1e07224268f56
DethR <Dethrocker13@yandex.ru>
2022-09-26 20:40:32 +0500

    9. Площадь треугольника. [#175474]

---
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double rsl = Math.sqrt((a + b + c) / 2 * (b / 2 + c / 2 - a / 2) * (a / 2 - b / 2 + c / 2) * (a / 2 + b / 2 - c / 2));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
