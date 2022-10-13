da43bb5aae5ee7489ce66433e707463807cb8662
DethR <Dethrocker13@yandex.ru>
2022-10-13 21:08:46 +0500

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/RollBackArray.java
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -4,9 +4,9 @@ public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int index  = 0; index < array.length; index++) {
-            int lastIndex = array.length - index - 1;
-            int currentIndex = index;
-            result[currentIndex] = array[lastIndex];
+            int last = array.length - index - 1;
+            int current = index;
+            result[current] = array[last];
         }
         return result;
     }
